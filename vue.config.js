fae73f1c4da6aeb81c8ba5e0804067f363f323a7
Dmitriy S. Sinyavskiy <contact@r3code.ru>
2020-03-11 14:08:18 +0300

    fix(example): fixed serve of static files from public
    - Added CORS headers

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 const path = require("path");
 
-module.exports = {
+module.exports = {  
   configureWebpack: {
     resolve: {
       alias: {
-        "@": path.join(__dirname, "examples")
+        "@": path.join(__dirname, "example", "src")
       }
     },
     entry: {
-      app: path.join(__dirname, "examples", "main.js")
+      app: path.join(__dirname, "example", "src", "main.js")
+    },
+    
+  },
+
+  // Configure Webpack's dev server.
+  // https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md
+  devServer: {
+    contentBase:  path.join(__dirname, "example","public"),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
     }
   }
 };
