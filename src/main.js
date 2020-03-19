4d8112629b77cddc9be687164a47bce29cc61baf
Dmitriy S. Sinyavskiy <contact@r3code.ru>
2020-03-19 23:32:17 +0300

    chore: import vis style path replaced

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { DataSet, DataView } from "vis-network";
 import Network from "./components/Network.vue";
-import "../node_modules/vis-network/dist/vis-network.css";
+import 'vis-network/styles/vis-network.css';
+
 
 export { Network, DataSet, DataView };
