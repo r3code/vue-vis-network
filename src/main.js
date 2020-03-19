c1c16bc12446e2000943be32b5dcf963798adbeb
Dmitriy S. Sinyavskiy <contact@r3code.ru>
2020-03-20 00:30:53 +0300

    style: fixed lint warning

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { DataSet, DataView } from "vis-network";
 import Network from "./components/Network.vue";
-import 'vis-network/styles/vis-network.css';
+import "vis-network/styles/vis-network.css";
 
 
 export { Network, DataSet, DataView };
